cf1556a08dcb9299b8c63fc85ab2f43a18ff3c59
inCBle <1374902847@qq.com>
2025-06-19 10:38:58 +0800

    build: 修改 prettier、eslint 规则配置，补充 react-x react-dom 依赖等
    1. 新增 vite-plugin-checker、eslint-plugin-react-x、eslint-plugin-react-dom、vite-plugin-checker 依赖
    
    2. README.md 补充技术栈列表
    
    3. 添加 .vscode/settings.json

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Beachburg-Admin
+
 比奇堡考勤管理后台
+
+## 技术栈
+
+1. React 19
+2. Antd-Design V5
+3. TypeScript
+4. Eslint
+5. Prettier
+6. React-Router-Dom
+7. Zustand
+8. Tailwind Css
+9. ahooks
--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,66 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+
+// react 相关插件
+import reactX from 'eslint-plugin-react-x';
+import reactDom from 'eslint-plugin-react-dom';
+
+// prettier 相关插件
+import prettierConfig from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended, eslintPluginPrettierRecommended],
+    extends: [js.configs.recommended, ...tseslint.configs.recommendedTypeChecked, prettierConfig],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser
+      globals: globals.browser,
+      parserOptions: {
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
     plugins: {
       'react-hooks': reactHooks,
-      'react-refresh': reactRefresh
+      'react-refresh': reactRefresh,
+      'react-x': reactX,
+      'react-dom': reactDom,
+      prettier: prettierPlugin,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }]
-    }
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      ...reactX.configs['recommended-typescript'].rules,
+      ...reactDom.configs.recommended.rules,
+
+      // 添加 prettier 规则，将格式问题报告为错误
+      'prettier/prettier': 'error',
+      'no-console': 'off',
+      'no-debugger': 'off',
+      'max-len': 'off',
+      'no-multi-spaces': 'off', // 由 prettier 处理
+      'no-return-assign': 'off',
+      semi: 'off', // 由 prettier 处理
+      eqeqeq: 'error',
+      'jsx-quotes': 'off', // 由 prettier 处理
+      'import/prefer-default-export': 'off',
+      'import/extensions': 'off',
+      'import/no-unresolved': 'off',
+      'no-multiple-empty-lines': 'off', // 由 prettier 处理
+      'no-param-reassign': 'off',
+      '@typescript-eslint/no-shadow': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/ban-ts-comment': 'off',
+      '@typescript-eslint/indent': 'off', // 由 prettier 处理
+      '@typescript-eslint/no-empty-object-typ': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+    },
   }
-)
+);
